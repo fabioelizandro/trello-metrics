c1084bcbb6520af4cf28feda8a1db2cdc9c5432e
Fabio Elizandro <fabio@elizandro.com.br>
2021-11-19 16:48:19 +1100

    feat: create histogram for all cards in the done list

--- go.mod
+++ go.mod
@@ -3,8 +3,12 @@ module trello-metrics
 go 1.17
 
 require (
-	github.com/adlio/trello v1.9.0 // indirect
-	github.com/fabioelizandro/goenv v0.0.1 // indirect
+	github.com/adlio/trello v1.9.0
+	github.com/fabioelizandro/goenv v0.0.1
+	github.com/go-echarts/go-echarts/v2 v2.2.4
+)
+
+require (
 	github.com/pkg/errors v0.8.1 // indirect
 	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e // indirect
 )
--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"os"
+	"sort"
+	"strconv"
 	"trello-metrics/kanban"
 
 	"github.com/fabioelizandro/goenv"
+	"github.com/go-echarts/go-echarts/v2/charts"
+	"github.com/go-echarts/go-echarts/v2/opts"
 )
 
 func main() {
@@ -27,7 +31,43 @@ func main() {
 		panic(err)
 	}
 
+	histogram := map[int]int{}
 	for _, card := range cards {
-		fmt.Printf("The card %s took %d days to get done\n", card.Name, card.DurationInDays)
+		if card.DurationInDays >= 0 {
+			histogram[card.DurationInDays]++
+		}
+	}
+	xAxisInt := []int{}
+	for durationInDays := range histogram {
+		xAxisInt = append(xAxisInt, durationInDays)
+	}
+	sort.Ints(xAxisInt)
+
+	bar := charts.NewBar()
+	bar.SetGlobalOptions(charts.WithTitleOpts(opts.Title{
+		Title:    "Histogram",
+		Subtitle: "Shows the duration distribution of done cards",
+	}))
+
+	xAxis := []string{}
+	for _, i := range xAxisInt {
+		xAxis = append(xAxis, strconv.Itoa(i))
+	}
+
+	bar.SetXAxis(xAxis)
+	yAxis := []opts.BarData{}
+	for _, i := range xAxisInt {
+		yAxis = append(yAxis, opts.BarData{Value: histogram[i]})
+	}
+	bar.AddSeries("Done", yAxis)
+
+	f, err := os.Create("bar.html")
+	if err != nil {
+		panic(err)
+	}
+
+	err = bar.Render(f)
+	if err != nil {
+		panic(err)
 	}
 }
